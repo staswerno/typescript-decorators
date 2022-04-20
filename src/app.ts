a31e8b7992ceeccdcf6388fad43d360c6c9c5606
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-04-20 09:12:21 +0200

    add decorator factory

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,26 @@
 // a decorator is ultimately a function 
 // applied to something, eg a class
 
-function Logger(constructor: Function) {
-    console.log('Logging...');
+// function Logger(constructor: Function) {
+//     console.log('Logging...');
+//     console.log(constructor);
+// }
+
+// converted to decorator factory
+
+function Logger(logString: string) {
+    return function (constructor: Function)
+  {  console.log(logString);
     console.log(constructor);
+}
 }
 
 // @ points at, not excecutes, decorator
 // decorators excecute when class is defined NOT instantiated
+// called using factory
 
-@Logger
+// @Logger
+@Logger('LOGGING - PERSON')
 class Person {
     name = 'Stasi';
 
