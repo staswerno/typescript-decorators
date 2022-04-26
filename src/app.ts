29c7aaf130adc9703270f096aac958f22f065e8d
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-04-26 13:26:40 +0200

    add accessor and parameter decorators

--- src/app.ts
+++ src/app.ts
@@ -55,11 +55,37 @@ function Log(target: any, propertyName: string | symbol) {
 	console.log(target, propertyName);
 }
 
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+	console.log("Accesor decorator!");
+	console.log(target);
+	console.log(name);
+	console.log(descriptor);
+}
+
+function Log3(
+	target: any,
+	name: string | Symbol,
+	descriptor: PropertyDescriptor
+) {
+	console.log("Method decorator!");
+	console.log(target);
+	console.log(name);
+	console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+	console.log("Parameter decorator!");
+	console.log(target);
+	console.log(name);
+	console.log(position);
+}
+
 class Product {
 	@Log
 	title: string;
 	private _price: number;
 
+	@Log2
 	set price(val: number) {
 		if (val > 0) {
 			this._price = val;
@@ -73,7 +99,8 @@ class Product {
 		this._price = p;
 	}
 
-	getPriceWithTax(tax: number) {
-		return this.price * (1 + tax);
+	@Log3
+	getPriceWithTax(@Log4 tax: number) {
+		return this._price * (1 + tax);
 	}
 }
