264dafdeb3de6e409c60b7f75a84576de44ba6ca
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-05-06 11:22:54 +0200

    returning (and changing) a class in a class decorator

--- src/app.ts
+++ src/app.ts
@@ -19,14 +19,20 @@ function Logger(logString: string) {
 // _ tells TS 'I'm aware it's there but i don't want to use it'
 
 function WithTemplate(template: string, hookId: string) {
-	return function (constructor: any) {
-		console.log("Rendering template");
-		const hookEl = document.getElementById(hookId);
-		const p = new constructor();
-		if (hookEl) {
-			hookEl.innerHTML = template;
-			hookEl.querySelector("h1")!.textContent = p.name;
-		}
+	return function <T extends { new (...args: any[]): { name: string } }>(
+		originalConstructor: T
+	) {
+		return class extends originalConstructor {
+			constructor(..._: any[]) {
+				super();
+				console.log("Rendering template");
+				const hookEl = document.getElementById(hookId);
+				if (hookEl) {
+					hookEl.innerHTML = template;
+					hookEl.querySelector("h1")!.textContent = this.name;
+				}
+			}
+		};
 	};
 }
 
